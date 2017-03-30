6160bbfd92486f3acfd8cb723dcdfcf437802cd8
IanRobinson01 <ianrobinson01@hotmail.com>
2017-03-30 23:05:38 +0100

    added in the anticipated detail of the Graph class based on explorer pseudo code and the provided searchexample package

--- src/main/java/student/Graph.java
+++ src/main/java/student/Graph.java
@@ -1,7 +1,35 @@
 package student;
 
-/**
- * Created by Ian Robinson on 30/03/2017.
- */
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 public class Graph {
+
+  private final List<GraphNode> nodesInGraph = new ArrayList<>();
+  private final Map<GraphNode, List<GraphNode>> nodeConnections = new ConcurrentHashMap<>();
+  private GraphNode root;
+
+  public GraphNode getRoot() {
+    return root;
+  }
+
+  public void setRoot(final GraphNode node) {
+    root = node;
+  }
+
+  public void addNode(final GraphNode node) {
+    nodesInGraph.add(node);
+  }
+
+  public void connectNode(final GraphNode parent, final GraphNode child) {
+    List<GraphNode> listOfChildNodes = nodeConnections.get(parent);
+    if (listOfChildNodes == null) {
+      listOfChildNodes = new ArrayList<>();
+    }
+    nodeConnections.put(parent, listOfChildNodes);
+    listOfChildNodes.add(child);
+  }
+
 }
