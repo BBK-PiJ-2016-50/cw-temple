d900d208c3388145301e0da64b8f55f7a51323f0
IanRobinson01 <ianrobinson01@hotmail.com>
2017-03-30 22:40:41 +0100

    provided code body for GraphNode based on what i anticipate will be required for the explore() method

--- src/main/java/student/GraphNode.java
+++ src/main/java/student/GraphNode.java
@@ -1,7 +1,55 @@
 package student;
 
-/**
- * Created by Ian Robinson on 30/03/2017.
- */
-public class Node {
+import game.NodeStatus;
+import java.util.Collection;
+
+public class GraphNode {
+
+  private long id;
+  private boolean hasBeenVisited = false;
+  private Collection<NodeStatus> neighbours;
+  private int distanceToOrb;
+
+  public GraphNode(long id, 
+                   boolean hasBeenVisited,
+                   Collection<NodeStatus> neighbours,
+                   int distanceToOrb) {
+    this.id = id;
+    this.hasBeenVisited = hasBeenVisited;
+    this.neighbours = neighbours;
+    this.distanceToOrb = distanceToOrb;
+  }
+
+  public void setId(long id) {
+    this.id = id;
+  }
+
+  public void setHasBeenVisited(boolean hasBeenVisited) {
+    this.hasBeenVisited = hasBeenVisited;
+  }
+
+  public void setNeighbours(Collection<NodeStatus> neighbours) {
+    this.neighbours = neighbours;
+  }
+
+  public void setDistanceToOrb(int distanceToOrb) {
+    this.distanceToOrb = distanceToOrb;
+  }
+
+  public long getId() {
+    return id;
+  }
+
+  public boolean getHasBeenVisited() {
+    return hasBeenVisited;
+  }
+
+  public Collection<NodeStatus> getNeighbours() {
+    return neighbours;
+  }
+
+  public int getDistanceToOrb() {
+    return distanceToOrb;
+  }
+
 }
