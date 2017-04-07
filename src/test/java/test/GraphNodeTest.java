e4d45c6db54bc22153c3fef2a9474460d8acaaa8
IanRobinson01 <ianrobinson01@hotmail.com>
2017-04-07 19:12:22 +0100

    added unit tests for GraphNode class

--- src/test/java/test/GraphNodeTest.java
+++ src/test/java/test/GraphNodeTest.java
@@ -1,7 +1,44 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import student.GraphNode;
+
 /**
- * Created by Ian Robinson on 07/04/2017.
+ * Unit tests for {@see GraphNode}.
+ *
+ * @author Ian Robinson
  */
 public class GraphNodeTest {
+
+  private GraphNode node;
+
+  @Before
+  public void setUp() {
+     node = new GraphNode(56, 15, false);
+  }
+
+  @Test
+  public void testSetHasBeenVisited() {
+    node.setHasBeenVisited(true);
+    assertEquals(true, node.getHasBeenVisited());
+  }
+
+  @Test
+  public void testGetNodeId() {
+    assertEquals(56, node.getNodeId());
+  }
+
+  @Test
+  public void testGetDistanceToOrb() {
+    assertEquals(15, node.getDistanceToOrb());
+  }
+
+  @Test
+  public void testGetHasBeenVisited() {
+    assertEquals(false, node.getHasBeenVisited());
+  }
+
 }
