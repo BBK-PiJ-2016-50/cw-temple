6856601e5f9ee7c5bccd663c2110e7f27f74041a
IanRobinson01 <ianrobinson01@hotmail.com>
2017-04-11 21:38:23 +0100

    tidied up comments for GraphNodeImpl class

--- src/main/java/student/GraphNodeImpl.java
+++ src/main/java/student/GraphNodeImpl.java
@@ -8,7 +8,7 @@ package student;
 public class GraphNodeImpl implements GraphNode {
 
   /**
-   * the unique long value assigned to the node.
+   * the unique long value id assigned to the node.
    */
   private final long nodeId;
 
@@ -23,14 +23,16 @@ public class GraphNodeImpl implements GraphNode {
   private boolean hasBeenVisited;
 
   /**
-   * constructor for generating the graph node.
+   * constructor for creating the graph node.
    * @param nodeId the unique id for the node.
    * @param distanceToOrb the distance the node is from the orb.
    * @param hasBeenVisited indicates if the node has been visited or not.
    */
-  public GraphNodeImpl(final long nodeId,
-                   final int distanceToOrb,
-                   final boolean hasBeenVisited) {
+  public GraphNodeImpl(
+          final long nodeId,
+          final int distanceToOrb,
+          final boolean hasBeenVisited
+  ) {
     this.nodeId = nodeId;
     this.distanceToOrb = distanceToOrb;
     this.hasBeenVisited = hasBeenVisited;
