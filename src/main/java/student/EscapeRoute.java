bb034f68e5e1151ce163fe8b06d3dce385799685
IanRobinson01 <ianrobinson01@hotmail.com>
2017-04-11 21:55:31 +0100

    tidied up comments for EscapeRoute interface

--- src/main/java/student/EscapeRoute.java
+++ src/main/java/student/EscapeRoute.java
@@ -3,13 +3,12 @@ package student;
 import game.EscapeState;
 import game.Node;
 
-import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
 
 /**
- * builds up a picture of the map for use when escaping.
- * uses Dijstra's algorithm to find shortest distance between two points.
+ * builds up a picture of the map for use when escaping using
+ * Dijkstra's algorithm.  This find the shortest distance between two points.
  *
  * @author Ian Robinson
  */
@@ -22,7 +21,8 @@ public interface EscapeRoute {
   void findRoute(Node startNode);
 
   /**
-   * uses the pathNodes map to construct the shortest path.
+   * uses a map of nodes to construct the shortest path from the StartNode
+   * specified in the findRoute method and the endNode specified in this method.
    * @param endNode the node which the explorer wants to get to.
    * @return the shortest route from start to exit as a list of nodes.
    */
@@ -31,9 +31,9 @@ public interface EscapeRoute {
   /**
    * allows the explorer to look around for gold on nodes that aren't
    * on the immediate escape route by checking neighbour nodes and their
-   * neighbour nodes.
+   * subsequent neighbour nodes.
    * a stack is maintained to ensure the explorer can find their way back
-   * to the main escape route path
+   * to the main escape route path.
    * @param state the information available at the current state.
    * @param pathToTake the escape route path
    * @param pathNode the current node from which the explorer will leave the
