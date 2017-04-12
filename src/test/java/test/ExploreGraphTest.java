86559312eb5cae74b0ddf12a649a1e63aa743a85
IanRobinson01 <ianrobinson01@hotmail.com>
2017-04-12 21:36:24 +0100

    refactored code to make use of mockito.  Used this to help test the addAndConnectNeighbours method where NodeStatus objects needed creating

--- src/test/java/test/ExploreGraphTest.java
+++ src/test/java/test/ExploreGraphTest.java
@@ -3,11 +3,24 @@ package test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+import game.Node;
+import game.NodeStatus;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
+import searchexample.Graph;
 import student.ExploreGraph;
+import student.ExploreGraphImpl;
 import student.GraphNode;
+import student.GraphNodeImpl;
+import org.junit.runner.RunWith;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 
@@ -16,28 +29,29 @@ import java.util.List;
  *
  * @author Ian Robinson
  */
+@RunWith(MockitoJUnitRunner.class)
 public class ExploreGraphTest {
 
   private ExploreGraph graph;
 
   @Before
   public void setUp() {
-    graph = new ExploreGraph();
+    graph = new ExploreGraphImpl();
   }
 
   @Test
   public void testAddNodes() {
-    graph.addNode(new GraphNode(10, 20, false));
-    graph.addNode(new GraphNode(15, 10, true));
-    graph.addNode(new GraphNode(100, 100, true));
+    graph.addNode(new GraphNodeImpl(10, 20, false));
+    graph.addNode(new GraphNodeImpl(15, 10, true));
+    graph.addNode(new GraphNodeImpl(100, 100, true));
     assertEquals(3, graph.getNodesInGraph().size());
   }
 
   @Test
   public void testConnectNode() {
-    GraphNode parent = new GraphNode(10, 20, false);
+    GraphNode parent = new GraphNodeImpl(10, 20, false);
+    GraphNode child = new GraphNodeImpl(15, 10, true);
     graph.addNode(parent);
-    GraphNode child = new GraphNode(15, 10, true);
     graph.addNode(child);
     graph.connectNode(parent, child);
     assertTrue(graph.getNodeConnections().containsKey(parent));
@@ -46,22 +60,22 @@ public class ExploreGraphTest {
 
   @Test
   public void testIdExists() {
-    graph.addNode(new GraphNode(100, 100, true));
+    graph.addNode(new GraphNodeImpl(100, 100, true));
     assertTrue(graph.idExists(100));
   }
 
   @Test
   public void testIdDoesntExist() {
-    graph.addNode(new GraphNode(100, 100, true));
+    graph.addNode(new GraphNodeImpl(100, 100, true));
     assertFalse(graph.idExists(50));
   }
 
   @Test
   public void testGetUnvisitedNeighbours() {
-    GraphNode nodeA = new GraphNode(100, 10, true);
-    GraphNode nodeB = new GraphNode(101, 15, false);
-    GraphNode nodeC = new GraphNode(102, 20, false);
-    GraphNode nodeD = new GraphNode(103, 5, true);
+    GraphNode nodeA = new GraphNodeImpl(100, 10, true);
+    GraphNode nodeB = new GraphNodeImpl(101, 15, false);
+    GraphNode nodeC = new GraphNodeImpl(102, 20, false);
+    GraphNode nodeD = new GraphNodeImpl(103, 5, true);
     graph.connectNode(nodeA, nodeB);
     graph.connectNode(nodeA, nodeC);
     graph.connectNode(nodeA, nodeD);
@@ -71,10 +85,10 @@ public class ExploreGraphTest {
 
   @Test
   public void testNoUnvisitedNeighbours() {
-    GraphNode nodeA = new GraphNode(100, 10, true);
-    GraphNode nodeB = new GraphNode(101, 15, true);
-    GraphNode nodeC = new GraphNode(102, 20, true);
-    GraphNode nodeD = new GraphNode(103, 5, true);
+    GraphNode nodeA = new GraphNodeImpl(100, 10, true);
+    GraphNode nodeB = new GraphNodeImpl(101, 15, true);
+    GraphNode nodeC = new GraphNodeImpl(102, 20, true);
+    GraphNode nodeD = new GraphNodeImpl(103, 5, true);
     graph.connectNode(nodeA, nodeB);
     graph.connectNode(nodeA, nodeC);
     graph.connectNode(nodeA, nodeD);
@@ -84,15 +98,40 @@ public class ExploreGraphTest {
 
   @Test
   public void testGetClosestNode() {
-    GraphNode nodeA = new GraphNode(100, 10, true);
-    GraphNode nodeB = new GraphNode(101, 15, false);
-    GraphNode nodeC = new GraphNode(102, 20, false);
-    GraphNode nodeD = new GraphNode(103, 5, false);
+    GraphNode nodeA = new GraphNodeImpl(100, 10, true);
+    GraphNode nodeB = new GraphNodeImpl(101, 15, false);
+    GraphNode nodeC = new GraphNodeImpl(102, 20, false);
+    GraphNode nodeD = new GraphNodeImpl(103, 5, false);
     graph.connectNode(nodeA, nodeB);
     graph.connectNode(nodeA, nodeC);
     graph.connectNode(nodeA, nodeD);
     List<GraphNode> unvNeighbours = graph.getUnvisitedNeighbours(nodeA);
     assertEquals(nodeD, graph.getClosestNode(unvNeighbours));
+  }
+
+  @Test
+  public void testAddAndConnectNeighbours() {
+    GraphNode nodeA = new GraphNodeImpl(1, 5, false);
+    graph.addNode(nodeA);
+    Collection<NodeStatus> neighbours = new ArrayList<>();
+    int total = 0;
+    long id = 20;
+    while (total < 5) {
+      NodeStatus neighbour = mock(NodeStatus.class);
+      neighbours.add(neighbour);
+      when(neighbour.getId()).thenReturn(id);
+      when(neighbour.getDistanceToTarget()).thenReturn(total + 1);
+      total++;
+      id++;
+    }
+    graph.addAndConnectNeighbours(nodeA, neighbours);
+
+    assertEquals(5, graph.getUnvisitedNeighbours(nodeA).size());
+    assertTrue(graph.idExists(21));
+    assertFalse(graph.idExists(30));
+    assertEquals(6, graph.getNodesInGraph().size());
+    assertEquals(1, graph.getNodeConnections().size());
+
   }
 
 }
