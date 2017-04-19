f0457bc4896c51846053404564b7b106e3001a21
IanRobinson01 <ianrobinson01@hotmail.com>
2017-04-19 22:54:37 +0100

    added tests for finding the exit when escaping

--- src/test/java/test/EscapeRouteTest.java
+++ src/test/java/test/EscapeRouteTest.java
@@ -6,6 +6,8 @@ import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+import game.EscapeState;
+import game.Node;
 import org.junit.Before;
 import org.junit.Test;
 import student.EscapeRoute;
@@ -13,6 +15,7 @@ import student.EscapeRouteImpl;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 
 /**
@@ -22,9 +25,42 @@ import java.util.List;
  */
 public class EscapeRouteTest {
 
+  private EscapeState escape;
+  private Node currentNode;
+  private Node exitNode;
+  private Collection<Node> vertices;
+
+  @Before
+  public void setUp() {
+    escape = mock(EscapeState.class);
+    currentNode = mock(Node.class);
+    exitNode = mock(Node.class);
+    vertices = new HashSet<>();
+  }
+
   //best gold route test
   //take route test
-  //exit found test
+
+  @Test
+  public void testExitFound() {
+    vertices.add(currentNode);
+    vertices.add(exitNode);
+    when(escape.getCurrentNode()).thenReturn(exitNode);
+    when(escape.getExit()).thenReturn(exitNode);
+    EscapeRoute route = new EscapeRouteImpl(currentNode, exitNode, vertices, 100);
+    assertTrue(route.exitFound(escape));
+  }
+
+  @Test
+  public void testExitNotFound() {
+    vertices.add(currentNode);
+    vertices.add(exitNode);
+    when(escape.getCurrentNode()).thenReturn(currentNode);
+    when(escape.getExit()).thenReturn(exitNode);
+    EscapeRoute route = new EscapeRouteImpl(currentNode, exitNode, vertices, 100);
+    assertFalse(route.exitFound(escape));
+  }
+
   //time to node test
   //add nodes to route test
   //collect gold test
