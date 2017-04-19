6088b1880d969e03482169e31b7c08eb82954a6c
IanRobinson01 <ianrobinson01@hotmail.com>
2017-04-19 22:21:49 +0100

    added skeleton test structure for EscapeRoute

---
+++ src/test/java/test/EscapeRouteTest.java
@@ -0,0 +1,32 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import org.junit.Before;
+import org.junit.Test;
+import student.EscapeRoute;
+import student.EscapeRouteImpl;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * Unit tests for {@see EscapeRoute}.
+ *
+ * @author Ian Robinson
+ */
+public class EscapeRouteTest {
+
+  //best gold route test
+  //take route test
+  //exit found test
+  //time to node test
+  //add nodes to route test
+  //collect gold test
+
+}
